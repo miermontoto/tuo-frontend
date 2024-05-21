0b8062566783044244b35651a2ede4d13e5b0a0b
Juan Mier <mier@mier.info>
2024-05-21 23:26:11 +0200

    correcicones y mejoras en llamadas a la API

--- src/helpers/Api.js
+++ src/helpers/Api.js
@@ -2,22 +2,23 @@ const API_URL = process.env.APP_URL || 'http://localhost'
 const API_PORT = process.env.APP_PORT || 3000
 const apiRoute = `${API_URL}:${API_PORT}`
 export const queryApi = async (method, endpoint, content = {}) => {
-	const url = `${apiRoute}/${endpoint}`
+	let url = `${apiRoute}/${endpoint}`
+
+	let params = {
+		method: method,
+		headers: {
+			'Content-Type': 'application/json',
+			'Authorization': `Bearer ${localStorage.getItem('token')}`,
+		},
+	}
+
+	if (Object.keys(content).length > 0) {
+		params.body = JSON.stringify(content)
+	}
 
 	try {
-		let response = await fetch(url, {
-			method: method,
-			headers: {
-				'Content-Type': 'application/json',
-				'Authorization': `Bearer ${localStorage.getItem('token')}`,
-			},
-			body: JSON.stringify(content),
-		});
-
-		if (!response.ok) {
-			throw new Error('Error en la petición')
-		}
-
+		let response = await fetch(url, params);
+		if (!response.ok) throw new Error('Error en la petición')
 		return await response.json()
 	} catch (error) {
 		console.log(error)
